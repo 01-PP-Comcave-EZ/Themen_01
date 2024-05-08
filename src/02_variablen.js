f666b147d92e87e5d7aa2d4b29324f6102b4e27c
gwen2345 <168723372+gwen2345@users.noreply.github.com>
2024-05-08 10:05:36 +0000

    Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -25,6 +25,7 @@
     JS ist ein untypisierte Sprache!
 */
 
+/*
 let test;
 console.log("Inhalt: "+test); // Ausgabe Inhalt
 console.log("Datentyp: "+typeof test); // Ausgabe Datentyp
@@ -44,4 +45,19 @@ test=true; // boolean
 console.log("Inhalt: "+test); // Ausgabe Inhalt
 console.log("Datentyp: "+typeof test); // Ausgabe Datentyp
 console.log("----------------------");
+*/
+
+/* Variablen vs. Konstanten in JS */
+
+// Variable 
+let test; // Variable : Deklaration
+test="Hi"; // Wertzuweisung (Initialisierung)
+test="Hello"; // Überschreiben
+console.log("Inhalt: "+test); // Ausgabe
+
+// Konstante
+const test2="Hi!"; // Deklaration + Wertzuweisung gemeinsam !
+test2="Huhu"; // Laufzeitfehler!! - run time error
+console.log("Inhalt: "+test2);
+
 
