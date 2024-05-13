a9b919381bcbfc56ab455e2c163d2bef65a8b9e6
Esther <esther.zirkler@gmail.com>
2024-05-13 15:28:22 +0200

    07_codechallenge_II_2

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -19,17 +19,27 @@ Alter 13-17 // Cola
 
 /******* Vorüberlegung / SWITCH|CASE 2 *******/
 
-const cond = true;
-const value = 2;
+// const cond = true;
+// const value = 2;
 
-switch (cond) {
-    case (value == 1):
-        console.log("Value hat den Wert 1.");
-        break;
-    case (value == 2):
-        console.log("Value hat den Wert 2.");
-        break;
-    default:
-        console.log("Value hat einen anderen Wert.");
-        break;
+// switch (cond) {
+//     case (value == 1):
+//         console.log("Value hat den Wert 1.");
+//         break;
+//     case (value == 2):
+//         console.log("Value hat den Wert 2.");
+//         break;
+//     default:
+//         console.log("Value hat einen anderen Wert.");
+//         break;
+// }
+let firstName = prompt("Vorname?: ");
+let age = prompt("Alter?: ");
+
+function outputNameAge(firstName, age) {
+    console.log(outputStr);
 }
+// const prompt = require('prompt-sync')({sigint: true});
+// outputNameAge(prompt("Vorname?: "),prompt("Alter?: "));
+
+
