897df5509f53635affd43338410f1de4dfdd7996
gwen2345 <168723372+gwen2345@users.noreply.github.com>
2024-05-15 14:26:47 +0000

    Arrays I komplett + Codechallange III

--- src/11_Arrays_I.js
+++ src/11_Arrays_I.js
@@ -6,97 +6,50 @@
  * 
  */
 
+/*** 02. Theorie: Array */
+
 // let arr;
-// // arr = new Array(); // Konstruktor
-// arr = [];
-// arr = [2,11,7,5,23,56,77] // Array mit Elementen
+// //arr = new Array(); // Konstruktor
+// // arr = [];
+// arr = [2,11,7,8,10]  // Array mit Elementen
 
-// output(arr);
+// output(arr); 
 // output(arr.length); // Anzahl der Elemente
-// output(arr[0]); // Index 0 (1. Pos. im Array)
-// output(arr[arr.length-1]); // Letzte Pos.
+// output(arr[0]) // Index 0 (1. Pos. im Array)
+// output(arr[arr.length-1]) // letze Pos. // allgemein .. n-1
 
+/* 02a. Theorie: Schleifen (for-schleife) */
+/* For -Schleifen als allg. Wiederholungs-Struktur */
 
-/**
- *   PRAXIS
- * 
- */
-
-
-/*** 01. Funktionalität mit Einzelparametern */
-// --> "Ich bin Max Mütze."
-// output(getSentence("Ich","bin","Max","Mütze"));
-// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
-// function getSentence(word1,word2,word3,word4,word5,word6) {
-//     const GAP = " ";
-//     const PUNCT = ".";
-//     let str =   word1 + GAP +
-//                 word2 + GAP +
-//                 word3 + GAP +
-//                 word4 + GAP +
-//                 word5 + GAP +
-//                 word6 +
-//                 PUNCT;
-
-//     return str;
+// Inkrement (untere Grenze --> obere Grenze)
+// for (let i = 0; i < 10; i++)  // i = Schleifenindex
+// {
+//     output("index: " + i);
 // }
 
-
-
-
-// Modul: Ausgabe in Konsole : Test
-// output("hi"); 
-function output(outputStr) {
-    console.log(outputStr);
-}
-
-/*** 01a. Funktionalität mit Array 1 */
-// Kritik | Überlegung
-// word1,word2, .... wordN :: Semantische Struktur
-// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
-// --> Transformation semantisches Problem --> numerisches Problem
-// output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
-// function getSentenceArr2(arr) {
-    // const GAP = " ";
-    // const PUNCT = ".";
-    // let str = "";
-//     for (let i=0; i < arr.length; i++) 
-//     {
-//         if (i==arr.length-1) 
-//         {
-//             str += arr[i] + PUNCT; // Ausnahme
-//         } else
-//         {
-//             str += arr[i] + GAP  // Regel
-//         }
-//     } 
-//     return str;   
+// Dekrement (obere Grenze --> untere Grenze)
+// for (let i = 10; i > 0; i--)  // i = Schleifenindex
+// {
+//     output("index: " + i);
 // }
 
-// 2 Variante: Struktur erzeugen + trimmen()
+// Inkrement (var. Schrittweite)
+// for (let i = 0; i <= 100; i += 5)  // i = Schleifenindex
+// {
+//     output("index: " + i);
+// }
 
-output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
-function getSentenceArr2(arr) 
-{
-    const GAP = " ";
-    const PUNCT = ".";
-    let str = "";
-//     for (let i=0; i < arr.length; i++) 
-//         { 
-//             str += arr[i] + GAP; 
-//             }
-//     return str.trim()+ PUNCT;
-//  }   
+/* 02b. For-Schleife für Array-Index (Iteration)*/
 
-// 3. Variante: Ausnutzen von generischen Funktionen...
-return arr.join(GAP)+PUNCT;
-}
-
-
-/***  */
+// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
+// for (let i = 0; i < arr.length; i++)  // i = Schleifenindex
+// {
+//     output("intern: " + i);
+//     output("extern: " + arr[i]); // Mapping
+// }
 
 /********* Überlegungen - Transponierung **********/
- 
+
 /*
 1. Einer Variablen kann Ihr eigener Wert zugewiesen werden.
 Solange die Variable existiert, bleibt dieser erhalten.
@@ -114,11 +67,11 @@ hier: Aufsummierung
 // output("inhalt von a: " + a);
 
 /* Besser: mit FOR-Schleife */
- 
+
 // let a = 0; // Anfangswert
-// for (let i=0; i < 5; i++) {
-//     a=a+1;
-//     output("Inhalt von a: "+a);
+// for (let i = 0; i < 5; i++) {
+//    a = a + 1;
+//    output("inhalt von a: " + a);
 // }
 
 /*
@@ -128,41 +81,95 @@ hier: Verkettung eines Strings // Transponierung
 */
 
 // let a = ""; // Anfangswert - leerer String
-// for (let i=0; i < 5; i++) {
-//     a=a+"Test";
-//     output("Inhalt von a: "+a);
+// for (let i = 0; i < 5; i++) {
+//    a = a + "Test";
+//    output("inhalt von a: " + a);
 // }
 
+/**
+ *   PRAXIS
+ * 
+ */
+
+/*** 01. Funktionalität mit Einzelparametern */
+// --> "Ich bin Max Mütze."
+// output(getSentence("Ich","bin","Max","Mütze"));
+// output(getSentence("Ich","bin","die","coole","Maxine","Mützerich"));
+function getSentence(word1,word2,word3,word4,word5,word6) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   word1 + GAP +
+                word2 + GAP +
+                word3 + GAP +
+                word4 + GAP +
+                word5 + GAP +
+                word6 +
+                PUNCT;
+
+    return str;
+}
+
+/*** 01a. Funktionalität mit Array 1 */
+// Kritik | Überlegung
+// word1,word2, .... wordN :: Semantische Struktur
+// arr[0],arr[1] ... arr[n-1] :: Numerische Struktur (0,1,2,3 ... n)
+// --> Transformation semantisches Problem --> numerisches Problem
+// output(getSentenceArr(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr(arr) {
+    const GAP = " ";
+    const PUNCT = ".";
+    let str =   arr[0] + GAP +
+                arr[1] + GAP +
+                arr[2] + GAP +
+                arr[3] + GAP +
+                arr[4] + GAP +
+                arr[5] +
+                PUNCT;
+
+    return str;
+}
+
+/*** 01b. Funktionalität mit Array 2  */
+// Transponierung:  untereinander ---> nebeneinander
+// Helge Schneider: Anananandereihung ...
+output(getSentenceArr2(["Ich","bin","die","coole","Maxine","Mützerich"]));
+function getSentenceArr2(arr) {
+
+    const GAP = " ";
+    const PUNCT = ".";
+    let str = "";
+
+    // // 1. Variante: Struktur GENAU erzeugen ...
+    // for (let i = 0; i < arr.length; i++)  // i = Schleifenindex
+    // {
+    //     if (i == arr.length-1) {
+    //         str += arr[i] + PUNCT; // Ausnahme 
+    //     } else {
+    //         str += arr[i] + GAP; // Regel
+    //     }
+    // }
+
+    // return str;
+
+    // // 2. Variante: Struktur erzeugen + trimmen()
+    // for (let i = 0; i < arr.length; i++)  // i = Schleifenindex
+    // {
+    //     str += arr[i] + GAP; // Regel
+    // }
+
+    // return str.trim() + PUNCT;
+
+    // 3. Variante: Ausnutzen von generischen Fuktionen ...
+    return arr.join(GAP) + PUNCT;
+
+}
 
 
 
 
-
-/* 02a. Theorie: Schleifen (for-schleife) */
-/* For -Schleifen als allg. Wiederholungs-Struktur */
-
-// for (let i = 0; i < 10; i++) // i = Schleifenindex // ++ Inkrement (untere Grenze --> obere Grenze)
-// {
-//     output("index: "+i);  
-// }
-
-// Dekrement (obere Grenze --> untere Grenze)
-
-// for (let i = 10; i > 0; i--)
-//     {
-//         output("index: "+i);  
-//     }
-
-// Inkrement (var. Schrittweite)
-// 
-
-/* 02b. For-Schleife für Array-Index (Iteration)*/
-
-// let arr = ["Ich","bin","die","coole","Maxine","Mützerich"];
-
-// for (let i = 0; i < arr.length; i++) 
-//     {
-//         output("intern: "+i);
-//         output("extern: "+arr[i]); // Mapping
-//     }
+// Modul: Ausgabe in Konsole : Test
+// output("hi"); 
+function output(outputStr) {
+    console.log(outputStr);
+}
 
